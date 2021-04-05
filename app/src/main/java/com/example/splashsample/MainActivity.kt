6fcf33b1d46ead58f188ae8595fee2c3f3404e1f
NIMOSAN <mogami0916r@gmail.com>
2021-04-06 00:42:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/splashsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.splashsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
