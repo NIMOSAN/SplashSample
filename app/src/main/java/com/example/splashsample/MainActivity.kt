1a53d65cc3b35537ec8d5e2b05e2d7ec6b9aac6a
NIMOSAN <mogami0916r@gmail.com>
2021-04-06 20:22:41 +0900

    edit

--- app/src/main/java/com/example/splashsample/MainActivity.kt
+++ app/src/main/java/com/example/splashsample/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.splashsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView2 = findViewById<TextView>(R.id.textView2);
+        blinkText(textView2, 1000, 500);
+    }
+
+    private fun blinkText(textView: TextView, duration: Long, offset: Long) {
+        val text:Animation = AlphaAnimation(0.0f, 1.0f)
+        text.duration = duration
+        text.startOffset = offset
+        text.repeatMode = Animation.REVERSE
+        text.repeatCount = Animation.INFINITE
+        textView.startAnimation(text)
     }
 }
